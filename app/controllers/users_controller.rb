ca28672b88b1abb8a32413e90ad0642fafaf9954
zdTang <zhendongtt@gmail.com>
2020-10-28 08:35:28 -0400

    before doing Authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,21 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    # Handle a successful update.
+    flash[:success] = "Profile updated"
+    redirect_to @user
+    else
+    render 'edit'
+    end
+  end
   
   private
 
