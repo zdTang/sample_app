0de5ea5babbb66ff86b206cb041c7be289b45e5a
zdTang <zhendongtt@gmail.com>
2020-10-28 20:15:59 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   #before_action :logged_in_user, only: [:edit, :update]
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  #before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   
   def index
     #@users = User.all
@@ -42,6 +44,12 @@ class UsersController < ApplicationController
     render 'edit'
     end
   end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
   
   private
 
@@ -63,8 +71,12 @@ class UsersController < ApplicationController
 # Confirms the correct user.
   def correct_user
   @user = User.find(params[:id])
-  redirect_to(root_url) unless current_user?(@user)
+    redirect_to(root_url) unless current_user?(@user)
   end
 
+# Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,11 @@
 User.create!(name: "Example User",
              email: "example@railstutorial.org",
              password:             "foobar",
-             password_confirmation: "foobar")
-    # Generate a bunch of additional users.
-    99.times do |n|
+             password_confirmation: "foobar",
+             admin: true)
+
+# Generate a bunch of additional users.
+99.times do |n|
     name = Faker::Name.name
     email = "example-#{n+1}@railstutorial.org"
     password = "password"
