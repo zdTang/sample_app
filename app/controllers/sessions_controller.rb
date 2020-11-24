9c9d095233f67f3bb302b05902f3450b5b50b351
zdTang <zhendongtt@gmail.com>
2020-11-24 10:46:29 -0500

    before 11.3.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
+
   def new
   end
-  
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
@@ -23,10 +23,9 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
-   
-  
-  def destroy  
-    log_out #if logged_in?
+
+  def destroy
+    log_out if logged_in?
     redirect_to root_url
   end
 end
