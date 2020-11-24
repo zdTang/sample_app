786030503b7012df0ced54fcffec65e915c56249
zdTang <zhendongtt@gmail.com>
2020-11-24 11:41:35 -0500

    modify user and password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app188966772'],
+    :password       => ENV['t733lih81863'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
