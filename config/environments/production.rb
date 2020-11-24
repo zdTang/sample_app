267ece026ae0015f8eb7b30f7f5a090ff94ab81e
zdTang <zhendongtt@gmail.com>
2020-11-24 12:07:59 -0500

    modify production.db about user password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app188966772@heroku.com'],
-    :password       => ENV['t733lih81863'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
