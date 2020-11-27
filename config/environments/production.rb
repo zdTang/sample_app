fd44ba88ba8117e57913964b774f5ddfbffefa4f
zdTang <zhendongtt@gmail.com>
2020-11-27 18:41:19 -0500

    modify production.rb port and authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
+    #:port           => '587',
+    :port           => '465',
+    #:authentication => :plain,
+    :authentication => :login,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
